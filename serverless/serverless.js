4a58c9aa4c02ab7e5dca2a520a60a280e4e9a16a
EHLabs <harishnk@EHLabss-iMac.local>
2017-05-05 23:35:50 -0400

    fixed res return

--- serverless/serverless.js
+++ serverless/serverless.js
@@ -6,7 +6,7 @@ var server = require('http').createServer(app);
 var bodyParser = require('body-parser');
 var request = require('request');
 
-const spawnSync = require('child_process').spawnSync;
+var spawnSync = require('child_process').spawnSync;
 
 app.get('/', function(req, res){
     res.send('hey!\n');
@@ -18,43 +18,59 @@ app.get('/run/:language', function(req, res){
     
     console.log(req.params.language)
 
-    if (req.params.language == 'Python') {
-      const command = 'python';
-      const extension = '.py'
-    } else if (req.params.language == 'NodeJS') {
-      const command = 'node';
-      const extension = '.js'
+    if (req.params.language == 'python') {
+      var command = 'python';
+      var extension = '.py';
+    } else if (req.params.language == 'nodejs') {
+      var command = 'node';
+      var extension = '.js';
+    } else {
+        var command = null;
+        var extension = null;
     }
 
     var filename = './code_to_run/lambda' + extension;
-    var spw = spawnSync(command, [filename]),
-    str = "";
+    var childProcess = spawnSync(command, [filename], {
+            cwd: process.cwd(),
+            input: null,
+            env: process.env,
+            stdio: 'pipe',
+            timeout: 50000,
+            killSignal: 'SIGTERM',
+            encoding: 'utf-8'});
 
-    spw.stdout.on('data', function (data) {
-        str += data.toString();
+    if (childProcess.status == 0) {
+        res.write(childProcess.stdout + "\n\n");
+    } else {
+        res.write(childProcess.stderr + "\n\n");
+    }
+    res.end(childProcess.status);
 
-        // just so we can see the server is doing something
-        console.log("data");
+    // spw.stdout.on('data', function (data) {
+    //     str += data.toString();
 
-        // Flush out line by line.
-        var lines = str.split("\n");
-        for(var i in lines) {
-            if(i == lines.length - 1) {
-                str = lines[i];
-            } else{
-                // Note: The double-newline is *required*
-                res.write('data: ' + lines[i] + "\n\n");
-            }
-        }
-    });
+    //     // just so we can see the server is doing something
+    //     console.log("data");
 
-    spw.on('close', function (code) {
-        res.end(str);
-    });
+    //     // Flush out line by line.
+    //     var lines = str.split("\n");
+    //     for(var i in lines) {
+    //         if(i == lines.length - 1) {
+    //             str = lines[i];
+    //         } else{
+    //             // Note: The double-newline is *required*
+    //             res.write('data: ' + lines[i] + "\n\n");
+    //         }
+    //     }
+    // });
 
-    spw.stderr.on('data', function (data) {
-        res.end('stderr: ' + data);
-    });
+    // spw.on('close', function (code) {
+    //     res.end(str);
+    // });
+
+    // spw.stderr.on('data', function (data) {
+    //     res.end('stderr: ' + data);
+    // });
 });
 
 //start the server
